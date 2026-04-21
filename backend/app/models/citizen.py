e0146d4c7e2220bb6203606ffcfc63a631a6a493
Bersan <bersankork@gmail.com>
2026-04-21 16:43:12 +0300

    The model name has been updated from User to Citizen.

---
+++ backend/app/models/citizen.py
@@ -0,0 +1,16 @@
+import uuid
+from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+from app.core.database import Base
+
+class Citizen(Base):
+    __tablename__ = "citizens"
+
+    # ER Diyagramındaki alanlar
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String(100), nullable=False)
+    emailAddress = Column(String(255), unique=True, index=True, nullable=False)
+    passwordHash = Column(String(255), nullable=False)
+    registrationDate = Column(DateTime, default=datetime.utcnow)
+    isActive = Column(Boolean, default=True)
